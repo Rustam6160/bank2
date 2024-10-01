f50159365150567633e3da664ae48e88111d76bb
Rustam Saidullaev <rhfkdg2@gmail.com>
2024-10-01 20:05:10 +0600

    t commit

--- bank/settings.py
+++ bank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t%j$4_*f)%gp&91yl(domb%6i=68yfvc&c&dej(s(0ro@q553d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
